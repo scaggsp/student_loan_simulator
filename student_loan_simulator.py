268b749c53d0c4cd26d9ecae526ba93bc2faa6c3
Phil Scaggs <scaggsp@users.noreply.github.com>
2018-02-28 10:51:55 -0500

    Move single period interest calculation to StudentLoan class

--- student_loan_simulator.py
+++ student_loan_simulator.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python
 """This script simlates month by month repayment of student loans."""
 
-def calculate_single_period_interest(principal, apr, periods):
-    """Calculate the compound interest accrued over a single period.
-
-    Keyword arguments:
-    principal -- principal at the start of the period
-    apr       -- Annual percentage Rate
-    periods   -- Number of times per year interest is calculated
-    """
-    apr_dec = apr / 100  # annual interest rate (decimal)
-    interest = principal * (1 + (apr_dec / periods)) - principal
-    return interest
-
 class StudentLoan:
     """Defines a student loan."""
     def __init__(self, principle, apr, compoundPeriods):
         self.principle = principle
         self.apr = apr
         self.compoundPeriods = compoundPeriods
+
+    def calculate_single_period_interest(self):
+        """Calculate the compound interest accrued over a single period."""
+        apr_dec = self.apr / 100  # annual interest rate (decimal)
+        interest = self.principle * (1 + (apr_dec / self.compoundPeriods)) - self.principle
+        return interest
--- test_student_loan_simulator_unittest.py
+++ test_student_loan_simulator_unittest.py
@@ -2,7 +2,6 @@
 """This script performs unit testing on the student_loan_simulator script.""" 
 
 import unittest
-from student_loan_simulator import calculate_single_period_interest
 from student_loan_simulator import StudentLoan
 
 dummyPrinciple = 0
@@ -13,14 +12,6 @@ class TestStudentLoanSimulator(unittest.TestCase):
 
     def setUp(self):
         pass
- 
-    def test_interest_calculator(self):
-        """Verify interst calculator function is accurate."""
-        princ = 10000         # starting principle
-        apr_perc = 10         # annual interest rate (percentage)
-        periods = 12          # compound periods per year
-        interest = calculate_single_period_interest(princ, apr_perc, periods)
-        self.assertAlmostEqual(interest, 83.33333333)
  
     def test_loan_class_create(self):
         """Create a loan class instance."""
@@ -59,6 +50,15 @@ class TestStudentLoanSimulator(unittest.TestCase):
         periods = 12  # compound periods per year
         loan = StudentLoan(dummyPrinciple, dummyApr, periods)
         self.assertEquals(loan.compoundPeriods, periods)
+
+    def test_loan_class_calculate_single_period_interest(self):
+        """StudentLoan class can calculate the loan's accrued interest from a single period."""
+        princ = 10000         # starting principle
+        apr_perc = 10         # annual interest rate (percentage)
+        periods = 12          # compound periods per year
+        loan = StudentLoan(princ, apr_perc, periods)
+        interest = loan.calculate_single_period_interest()
+        self.assertAlmostEqual(interest, 83.33333333)
 
 if __name__ == '__main__':
     unittest.main()
