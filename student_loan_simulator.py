9769faf1acb8ef2b8eb6717b80d5dd8987c39580
Phil Scaggs <scaggsp@users.noreply.github.com>
2018-03-08 09:57:30 -0500

    Drop compounding period in favor of hardcoded 1 month
    * Instead of calculating interest for a single period, a single month
      is used instead. Given that student loan payments are due every month,
      specifying the period is an unnecessary qualification.

--- student_loan_simulator.py
+++ student_loan_simulator.py
@@ -5,20 +5,20 @@ class StudentLoan:
     """Defines a student loan."""
     last_payment_details = {"payment": 0,"principleReduction": 0,"InterestPaid": 0}
 
-    def __init__(self, principle, apr, compoundPeriods):
+    def __init__(self, principle, apr):
         self.principle = principle
         self.apr = apr
-        self.compoundPeriods = compoundPeriods
 
-    def calculate_single_period_interest(self):
+    def calculate_single_month_interest(self):
         """Calculate the compound interest accrued over a single period."""
         apr_dec = self.apr / 100  # annual interest rate (decimal)
-        interest = self.principle * (1 + (apr_dec / self.compoundPeriods)) - self.principle
+        monthesPerYear = 12
+        interest = self.principle * (1 + (apr_dec / monthesPerYear)) - self.principle
         return interest
 
     def apply_payment(self, payment):
         self.last_payment_details["payment"] = payment
-        interest = self.calculate_single_period_interest()
+        interest = self.calculate_single_month_interest()
         self.last_payment_details["InterestPaid"] = interest
         self.last_payment_details["principleReduction"] = payment - interest
         self.principle -= payment - interest
--- test_student_loan_simulator_unittest.py
+++ test_student_loan_simulator_unittest.py
@@ -6,7 +6,6 @@ from student_loan_simulator import StudentLoan
 
 dummyPrinciple = 0
 dummyApr = 0
-dummyCompPeriods = 0
 
 class TestStudentLoanSimulator(unittest.TestCase):
 
@@ -15,84 +14,69 @@ class TestStudentLoanSimulator(unittest.TestCase):
  
     def test_loan_class_create(self):
         """Create a loan class instance."""
-        loan = StudentLoan(dummyPrinciple, dummyApr, dummyCompPeriods)
+        loan = StudentLoan(dummyPrinciple, dummyApr)
         self.assertIsInstance(loan, StudentLoan)
 
     def test_loan_class_has_principle_var(self):
         """StudentLoan class has a principal variable."""
-        loan = StudentLoan(dummyPrinciple, dummyApr, dummyCompPeriods)
+        loan = StudentLoan(dummyPrinciple, dummyApr)
         self.assertTrue(hasattr(loan, 'principle'))
 
     def test_loan_class_initializes_principle(self):
         """StudentLoan class initializes principal variable."""
         princ = 10000  # starting principle
-        loan = StudentLoan(princ, dummyApr, dummyCompPeriods)
+        loan = StudentLoan(princ, dummyApr)
         self.assertEqual(loan.principle, princ)
 
     def test_loan_class_has_apr_var(self):
         """StudentLoan class has a apr variable."""
-        loan = StudentLoan(dummyPrinciple, dummyApr, dummyCompPeriods)
+        loan = StudentLoan(dummyPrinciple, dummyApr)
         self.assertTrue(hasattr(loan, 'apr'))
 
     def test_loan_class_initializes_apr(self):
         """StudentLoan class initializes apr variable."""
         apr = 12  # Annual interest rate (percentage)
-        loan = StudentLoan(dummyPrinciple, apr, dummyCompPeriods)
+        loan = StudentLoan(dummyPrinciple, apr)
         self.assertEqual(loan.apr, apr)
 
-    def test_loan_class_has_compoundPeriods_var(self):
-        """StudentLoan class has a compoundPeriods variable."""
-        loan = StudentLoan(dummyPrinciple, dummyApr, dummyCompPeriods)
-        self.assertTrue(hasattr(loan, 'compoundPeriods'))
-
-    def test_loan_class_initializes_compoundPeriods(self):
-        """StudentLoan class initializes compoundPeriods variable."""
-        periods = 12  # compound periods per year
-        loan = StudentLoan(dummyPrinciple, dummyApr, periods)
-        self.assertEqual(loan.compoundPeriods, periods)
-
     def test_StudentLoan_classes_do_not_share_initial_resources(self):
-        loan1 = StudentLoan(2000, 6, 6)
-        loan2 = StudentLoan(4000, 12, 12)
+        loan1 = StudentLoan(2000, 6)
+        loan2 = StudentLoan(4000, 12)
         self.assertNotEqual(loan1.principle, loan2.principle)
         self.assertNotEqual(loan1.apr, loan2.apr)
-        self.assertNotEqual(loan1.compoundPeriods, loan2.compoundPeriods)
 
     def test_loan_class_calculate_single_period_interest(self):
         """StudentLoan class can calculate the loan's accrued interest from a single period."""
         princ = 10000         # starting principle
         apr_perc = 12         # annual interest rate (percentage)
-        periods = 12          # compound periods per year
-        loan = StudentLoan(princ, apr_perc, periods)
-        interest = loan.calculate_single_period_interest()
+        loan = StudentLoan(princ, apr_perc)
+        interest = loan.calculate_single_month_interest()
         self.assertEqual(interest, 100)
 
     def test_loan_class_has_apply_payment_method(self):
         """StudentLoan class has apply_payment method."""
-        loan = StudentLoan(dummyPrinciple, dummyApr, dummyCompPeriods)
+        loan = StudentLoan(dummyPrinciple, dummyApr)
         self.assertTrue(hasattr(loan, 'apply_payment'))
 
     def test_loan_apply_loan_payment(self):
         """apply_payment lowers principle correctly."""
         princ = 10000         # starting principle
         apr_perc = 12         # annual interest rate (percentage)
-        periods = 12          # compound periods per year
-        loan = StudentLoan(princ, apr_perc, periods)
+        loan = StudentLoan(princ, apr_perc)
         loan.apply_payment(200)
         # The payment should pay 100 to interest and the rest to principle
         self.assertEqual(loan.principle, 9900)
 
     def test_loan_class_has_last_payment_details_method(self):
         """StudentLoan class has last_payment_details method."""
-        loan = StudentLoan(dummyPrinciple, dummyApr, dummyCompPeriods)
+        loan = StudentLoan(dummyPrinciple, dummyApr)
         self.assertTrue(hasattr(loan, 'last_payment_details'))
 
     def test_verify_last_payment_details(self):
         """Verify last_payment_details accuracy."""
         princ = 10000         # starting principle
         apr_perc = 12         # annual interest rate (percentage)
-        periods = 12          # compound periods per year
-        loan = StudentLoan(princ, apr_perc, periods)
+        loan = StudentLoan(princ, apr_perc)
         loan.apply_payment(250)
         self.assertEqual(loan.last_payment_details["payment"], 250)
         self.assertEqual(loan.last_payment_details["principleReduction"], 150)
