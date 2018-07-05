35de78f0f9e9f524fd40fbc6cc675dfa05df372f
Phillip Scaggs <scaggsp@users.noreply.github.com>
2018-07-05 19:24:29 -0400

    Add StudentLoan class constructor
    StudentLoan Class
    * Change floats to doubles
    * Add principle property

--- StudentLoanSimulator/StudentLoanSimulator/StudentLoanSimulator.cs
+++ StudentLoanSimulator/StudentLoanSimulator/StudentLoanSimulator.cs
@@ -6,8 +6,25 @@ namespace StudentLoanSimulator
     {
         public String LenderName { get; set; }
         public String AccountNumber { get; set; }
-        public float APR { get; set; }
-        public float MinPayment { get; set; }
+        public double APR { get; set; }
+        public double MinPayment { get; set; }
         public DateTime PaymentStartDate { get; set; }
+        public double Principle { get; set; }
+
+        public StudentLoan(String lenderName,
+                           String accountNumber,
+                           double aPR,
+                           double minPayment,
+                           DateTime paymentStartDate,
+                           double startingPrinciple
+                           )
+        {
+            LenderName = lenderName;
+            AccountNumber = accountNumber;
+            APR = aPR;
+            MinPayment = minPayment;
+            PaymentStartDate = paymentStartDate;
+            Principle = startingPrinciple;
+        }
     }
 }
--- StudentLoanSimulator/StudentLoanSimulatorTests/StudentLoanClassUnitTests.cs
+++ StudentLoanSimulator/StudentLoanSimulatorTests/StudentLoanClassUnitTests.cs
@@ -7,7 +7,7 @@ namespace StudentLoanSimulatorTests
 {
     [TestClass]
     public class StudentLoanClassUnitTests
-    {
+    { 
         /// <summary>
         /// Student Loan Class has LenderName field
         /// </summary>
@@ -59,7 +59,7 @@ namespace StudentLoanSimulatorTests
             Assert.AreNotEqual(null, studentLoanPropInfo, "APR property does not exist in StudentLoan Class");
 
             // Verify property has the correct type
-            Assert.AreEqual(studentLoanPropInfo.PropertyType, typeof(float), "APR property is an incorrect type");
+            Assert.AreEqual(studentLoanPropInfo.PropertyType, typeof(double), "APR property is an incorrect type");
         }
 
         /// <summary>
@@ -77,7 +77,7 @@ namespace StudentLoanSimulatorTests
             Assert.AreNotEqual(null, studentLoanPropInfo, "MinPayment property does not exist in StudentLoan Class");
 
             // Verify property has the correct type
-            Assert.AreEqual(studentLoanPropInfo.PropertyType, typeof(float), "MinPayment property is an incorrect type");
+            Assert.AreEqual(studentLoanPropInfo.PropertyType, typeof(double), "MinPayment property is an incorrect type");
         }
 
         /// <summary>
@@ -96,6 +96,54 @@ namespace StudentLoanSimulatorTests
 
             // Verify property has the correct type
             Assert.AreEqual(studentLoanPropInfo.PropertyType, typeof(DateTime), "PaymentStartDate property is an incorrect type");
+        }
+
+        /// <summary>
+        /// Student Loan Class has Principle field
+        /// </summary>
+        [TestMethod]
+        public void TestHasPrincipleProperty()
+        {
+            // Get the Type object corresponding to MyClass.
+            Type studentLoanType = typeof(StudentLoan);
+            // Get the PropertyInfo object by passing the property name.
+            PropertyInfo studentLoanPropInfo = studentLoanType.GetProperty("Principle");
+
+            // Verify property exists
+            Assert.AreNotEqual(null, studentLoanPropInfo, "Principle property does not exist in StudentLoan Class");
+
+            // Verify property has the correct type
+            Assert.AreEqual(studentLoanPropInfo.PropertyType, typeof(double), "Principle property is an incorrect type");
+        }
+
+        /// <summary>
+        /// Student Loan Class consturcor is called without throwing an exception.
+        /// The constuctor values are read back out of the new object for correctness.
+        /// </summary>
+        [TestMethod]
+        public void TestConstructor()
+        {
+            String testLenderName = "testName";
+            String testAccountNumber = "ABC-123456";
+            double testAPR = 0.0325;
+            double testMinPayment = 25.73;
+            DateTime testPaymentStartDate = DateTime.Now;
+            double testStartingPrinciple = 4001.00;
+
+            StudentLoan testStudentLoan = new StudentLoan(testLenderName,
+                                                          testAccountNumber,
+                                                          testAPR,
+                                                          testMinPayment,
+                                                          testPaymentStartDate,
+                                                          testStartingPrinciple
+                                                          );
+
+            Assert.AreEqual(testLenderName, testStudentLoan.LenderName);
+            Assert.AreEqual(testAccountNumber, testStudentLoan.AccountNumber);
+            Assert.AreEqual(testAPR, testStudentLoan.APR);
+            Assert.AreEqual(testMinPayment, testStudentLoan.MinPayment);
+            Assert.AreEqual(testPaymentStartDate, testStudentLoan.PaymentStartDate);
+            Assert.AreEqual(testStartingPrinciple, testStudentLoan.Principle);
         }
     }
 }
